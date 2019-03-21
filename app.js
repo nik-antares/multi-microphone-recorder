14eafcf2b5f274217fde8b60cc8601b981667566
Nikhil <garg@qtsolv.com>
2019-03-22 00:05:53 +0530

    added single record and stop button for multiple microphones

--- app.js
+++ app.js
@@ -11,16 +11,22 @@ stop.disabled = true;
 
 if (navigator.mediaDevices.getUserMedia) {
 	console.log('getUserMedia supported.');
+	let record    = document.querySelector('.record');
+	let stop      = document.querySelector('.stop');
 
-	var onSuccess = function(stream, chunks, i) {
-		var mediaRecorder = new MediaRecorder(stream);
-		var record        = document.querySelector('.record' + i);
-		var stop          = document.querySelector('.stop' + i);
+	let mediaRecorders = [];
+	let chunks_array   = [];
+
+	var onSuccess = function(stream) {
+		mediaRecorders.push (new MediaRecorder(stream));
 
 		record.onclick = function() {
-			mediaRecorder.start();
+			mediaRecorders.forEach ((mediaRecorder) => { 
+				mediaRecorder.start(); 
 
-			console.log(mediaRecorder.state);
+				console.log(mediaRecorder.state);
+			});
+
 			console.log("recorder started");
 
 			record.style.background = "red";
@@ -30,9 +36,12 @@ if (navigator.mediaDevices.getUserMedia) {
 		}
 
 		stop.onclick = function() {
-			mediaRecorder.stop();
+			mediaRecorders.forEach ((mediaRecorder) => { 
+				mediaRecorder.stop();
 
-			console.log(mediaRecorder.state);
+				console.log(mediaRecorder.state);
+			});
+
 			console.log("recorder stopped");
 
 			record.style.background = "";
@@ -43,61 +52,66 @@ if (navigator.mediaDevices.getUserMedia) {
 			record.disabled = false;
 		}
 
-		mediaRecorder.onstop = function(e) {
-			console.log("data available after MediaRecorder.stop() called.");
+		mediaRecorders.forEach ((mediaRecorder, index) => {
+			mediaRecorder.onstop = function(e) {
+				console.log("data available after MediaRecorder.stop() called.");
 
-			var clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
+				var clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
 
-			console.log(clipName);
+				console.log(clipName);
 
-			var clipContainer = document.createElement('article');
-			var clipLabel     = document.createElement('p');
-			var audio         = document.createElement('audio');
-			var deleteButton  = document.createElement('button');
+				var clipContainer = document.createElement('article');
+				var clipLabel     = document.createElement('p');
+				var audio         = document.createElement('audio');
+				var deleteButton  = document.createElement('button');
 
-			clipContainer.classList.add('clip');
-			audio.setAttribute('controls', '');
-			deleteButton.textContent = 'Delete';
-			deleteButton.className = 'delete';
+				clipContainer.classList.add('clip');
+				audio.setAttribute('controls', '');
+				deleteButton.textContent = 'Delete';
+				deleteButton.className = 'delete';
 
-			if (clipName === null)
-				clipLabel.textContent = 'My unnamed clip';
-			else
-				clipLabel.textContent = clipName;
+				if (clipName === null)
+					clipLabel.textContent = 'My unnamed clip';
+				else
+					clipLabel.textContent = clipName;
 
-			clipContainer.appendChild(audio);
-			clipContainer.appendChild(clipLabel);
-			clipContainer.appendChild(deleteButton);
-			soundClips.appendChild(clipContainer);
+				clipContainer.appendChild(audio);
+				clipContainer.appendChild(clipLabel);
+				clipContainer.appendChild(deleteButton);
+				soundClips.appendChild(clipContainer);
 
-			audio.controls = true;
-			var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+				audio.controls = true;
+				var blob = new Blob(chunks_array[index], { 'type' : 'audio/ogg; codecs=opus' });
 
-			chunks = [];
+				chunks_array[index] = [];
 
-			var audioURL = window.URL.createObjectURL(blob);
-			audio.src = audioURL;
-			console.log("recorder stopped");
+				var audioURL = window.URL.createObjectURL(blob);
+				audio.src = audioURL;
+				console.log("recorder stopped");
 
-			deleteButton.onclick = function(e) {
-				evtTgt = e.target;
-				evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-			}
+				deleteButton.onclick = function(e) {
+					evtTgt = e.target;
+					evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+				}
 
-			clipLabel.onclick = function() {
-				var existingName = clipLabel.textContent;
-				var newClipName = prompt('Enter a new name for your sound clip?');
-				if(newClipName === null) {
-					clipLabel.textContent = existingName;
-				} else {
-					clipLabel.textContent = newClipName;
+				clipLabel.onclick = function() {
+					var existingName = clipLabel.textContent;
+					var newClipName = prompt('Enter a new name for your sound clip?');
+					if(newClipName === null) {
+						clipLabel.textContent = existingName;
+					} else {
+						clipLabel.textContent = newClipName;
+					}
 				}
 			}
-		}
+		});
 
-		mediaRecorder.ondataavailable = function(e) {
-			chunks.push(e.data);
-		}
+		mediaRecorders.forEach ((mediaRecorder, index) => {
+			mediaRecorder.ondataavailable = function(e) {
+				chunks_array[index] = [];
+				chunks_array[index].push(e.data);
+			}
+		});
 	}
 
 	var onError = function(err) {
@@ -106,31 +120,14 @@ if (navigator.mediaDevices.getUserMedia) {
 
 	navigator.mediaDevices.enumerateDevices().then(
 		function (dev) {
-			let audioSelect = {};
-			let count = 0;
-
 			for (let i = 0; i < dev.length; i++) {
 				if (dev[i].kind == 'audioinput' && dev[i].deviceId !== 'default') {
-					audioSelect['value'] = dev[i].deviceId;
-
-					var chunks      = [[], []];
-					var constraints = { 
-						audio: {
-							deviceId: {
-								exact: audioSelect.value
-							}
-						}
+					let deviceId    = dev[i].deviceId;
+					let constraints = { 
+						audio: { deviceId }
 					};
 
-					navigator.mediaDevices.getUserMedia(constraints).then(
-						function (stream) {
-							count++;
-							onSuccess (stream, chunks[count -1], count);
-						},
-						function (err) {
-							onError (err);
-						}
-					);
+					navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
 				}
 			}
 		});
